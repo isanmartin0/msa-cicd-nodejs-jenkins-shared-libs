650aa4cfb1d1e10acd686bbba8d7802bd6be5594
isanmartin <isanmartin@keedio.com>
2018-05-16 20:49:14 +0200

    Added nodejsOpenshiftCheckAndCreateProject global variable

---
+++ vars/nodejsOpenshiftCheckAndCreateProject.groovy
@@ -0,0 +1,63 @@
+#!/usr/bin/groovy
+import com.evobanco.NodejsConstants
+
+def call(body) {
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+    def packageJSON = readJSON file: 'package.json'
+    def project = "${packageJSON.name}"
+    def projectName
+    def branchNameContainerImage = ""
+    int minimumPodReplicas = Constants.MINIMUM_POD_REPLICAS
+    int maximumPodReplicas = Constants.MAXIMUM_POD_REPLICAS
+    def hostname = ""
+    if (config.branch_type == 'master') {
+    	projectName = "${packageJSON.name}"
+
+        //Host name for the route element
+        hostname = ".svcs" + Constants.HOSTNAME_DOMAIN
+
+        //Set minimum number of replicas of pod
+        minimumPodReplicas = Constants.MINIMUM_POD_REPLICAS_PRO_ENVIRONMENT
+        maximumPodReplicas = Constants.MAXIMUM_POD_REPLICAS_PRO_ENVIRONMENT
+
+    } else {
+    	projectName = "${packageJSON.name}-${config.branchHY}"
+
+        //Branch name for image container
+        branchNameContainerImage = "-${config.branchHY}"
+        //Host name for the route element
+        hostname = "-${config.branchHY}.svcs${config.environment}" + Constants.HOSTNAME_DOMAIN
+    }
+
+    withCredentials([usernamePassword(credentialsId: "${config.oseCredential}", passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {
+        sh "oc login ${config.cloudURL} -u ${USERNAME} -p ${PASSWORD} --insecure-skip-tls-verify=true"
+    }
+
+    try {
+        sh "oc project ${projectName}"
+    } catch (err) {
+
+        println err
+
+        echo "As the project does not exist on openshift, we will create it"
+
+        sh "oc new-project ${projectName}"
+        sh "oc label namespace ${projectName} \"environment=${config.environment}\""
+        sh "oc policy add-role-to-user view system:serviceaccount:${config.jenkinsNS}:jenkins -n ${projectName}"
+        sh "oc policy add-role-to-user edit system:serviceaccount:${config.jenkinsNS}:jenkins -n ${projectName}"
+
+        sh "oc project ${projectName}"
+
+        withCredentials([string(credentialsId: "${config.artCredential}", variable: 'ARTIFACTORY_TOKEN')]) {
+            sh "oc process -n ${projectName} -f ${config.template} BRANCH_NAME=${env.BRANCH_NAME} BRANCH_NAME_HY=${config.branchHY} BRANCH_NAME_HY_CONTAINER_IMAGE=${branchNameContainerImage} " +
+                    "PROJECT=${project} DOCKER_REGISTRY=${config.dockerRegistry} ARTIFACTORY_TOKEN=${ARTIFACTORY_TOKEN} SOURCE_REPOSITORY_URL=${config.sourceRepositoryURL} " +
+                    "SOURCE_REPOSITORY_BRANCH=${config.sourceRepositoryBranch} NPM_MIRROR=${config.npmMirror} NODEJS_VERSION=${config.nodejsVersion} envLabel=${config.environment} HOST_NAME=${hostname} MIN_POD_REPLICAS=${minimumPodReplicas} MAX_POD_REPLICAS=${maximumPodReplicas}| oc create -n ${projectName} -f -"
+        }
+
+        echo "Resources (is,bc,dc,svc,secret,route) created under OCP namespace ${projectName}"
+    }
+}
