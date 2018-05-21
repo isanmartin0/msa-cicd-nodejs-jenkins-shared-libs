84ef39e2d91a81ae4941488e9a60ec7b326829eb
isanmartin <isanmartin@keedio.com>
2018-05-21 15:32:55 +0200

    Added creation PORT environment variable

--- vars/nodejsOpenshiftEnvironmentVariables.groovy
+++ vars/nodejsOpenshiftEnvironmentVariables.groovy
@@ -7,8 +7,17 @@ def call(body) {
     body.delegate = config
     body()
 
+    Boolean isCreatePortEnvironmentVariable = false
+    def createPortEnvironmentVariable = config.createPortEnvironmentVariableOpenshift
+    def portNumber = config.portNumber
     def branchNameHY = config.branchHY
     def branchType = config.branch_type
+
+    echo "createPortEnvironmentVariable: ${createPortEnvironmentVariable}"
+    echo "portNumber: ${portNumber}"
+    echo "branchNameHY: ${branchNameHY}"
+    echo "branchType: ${branchType}"
+
 
     def packageJSON = readJSON file: 'package.json'
     def project = "${packageJSON.name}"
@@ -22,5 +31,22 @@ def call(body) {
 
     echo "Creating environment variables"
 
+    if (createPortEnvironmentVariable != null) {
+        isCreatePortEnvironmentVariable = createPortEnvironmentVariable.toBoolean()
+    }
+
+    if (isCreatePortEnvironmentVariable) {
+        try {
+            //Remove JAVA_OPTS environment variable created by template
+            echo "Removing PORT environment variable"
+            sh "oc env dc/${project} PORT- -n ${projectName}"
+        } catch (err) {
+            echo "The PORT environment variable on dc/${project} -n ${projectName} cannot be removed"
+        }
+
+        echo "Adding PORT=${portNumber} environment variable"
+        sh "oc env dc/${project} PORT=\"${portNumber}\" -n ${projectName}"
+
+    }
 
 }
