894900571d5b995e2243ddcdbd690dfef65b6998
isanmartin <isanmartin@keedio.com>
2018-05-25 09:07:49 +0200

    Modified global variable

--- vars/nodejsOpenshiftBuildProject.groovy
+++ vars/nodejsOpenshiftBuildProject.groovy
@@ -9,6 +9,8 @@ def call(body) {
 
 
     echo "config.repoUrl: ${config.repoUrl}"
+    echo "config.package_tag: ${config.package_tag}"
+    echo "config.is_scoped_package: ${config.is_scoped_package}"
 
     def packageJSON = readJSON file: 'package.json'
     def project = "${packageJSON.name}"
@@ -22,7 +24,7 @@ def call(body) {
     def version = packageJSON.version
 
     def artifactUrl = "${config.repoUrl}/${packageJSON.name}/${version}/${packageJSON.name}-${version}.tgz"
-    def buildEnvVars = [ [ name : 'WAR_FILE_URL', value : artifactUrl ]]
+    def buildEnvVars = [ [ name : 'WAR_FILE_URL', value : artifactUrl ], [ name: 'PACKAGE_TAG', value: config.package_tag], [ name: 'IS_SCOPED_PACKAGE', value: config.is_scoped_package]]
 
     openshiftBuild buildConfig: project, namespace: projectName, verbose: 'true', showBuildLogs: 'true', env: buildEnvVars
 
