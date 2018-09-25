23ae0471024ec62ea54f1b3d2de84e7278a4fa22
isanmartin <isanmartin@keedio.com>
2018-09-25 08:18:49 +0200

    Added build env vars (NODEJS_PACKAGE_TAG, NODEJS_PACKAGE_TARBALL and NODEJS_IS_SCOPED_PACKAGE) to build process

--- vars/nodejsOpenshiftBuildProject.groovy
+++ vars/nodejsOpenshiftBuildProject.groovy
@@ -20,11 +20,16 @@ def call(body) {
     echo "config.npmMirrorOpenshift:  ${config.npmMirrorOpenshift}"
     echo "config.useAlternateNpmRunOpenshift:  ${config.useAlternateNpmRunOpenshift}"
     echo "config.alternateNpmRunScriptOpenshift:  ${config.alternateNpmRunScriptOpenshift}"
+    echo "config.package_tag:  ${config.package_tag}"
+    echo "config.package_tarball:  ${config.package_tarball}"
+    echo "config.is_scoped_package:  ${config.is_scoped_package}"
+
 
 
     Boolean isDevMode = false
     Boolean isUseNpmMirror = false
     Boolean isUseAlternateNpmRunScript = false
+    Boolean isScopedPackage = false
 
     def devMode = config.devModeOpenshift
     def debugPort = config.debugPortOpenshift
@@ -32,7 +37,7 @@ def call(body) {
     def npmMirror = config.npmMirrorOpenshift
     def useAlternateNpmRun = config.useAlternateNpmRunOpenshift
     def alternateNpmRunScript = config.alternateNpmRunScriptOpenshift
-
+    def scopedPackage = config.is_scoped_package
 
     def packageJSON = readJSON file: 'package.json'
     def project = utils.getProject(packageJSON.name)
@@ -56,14 +61,19 @@ def call(body) {
         isUseAlternateNpmRunScript = useAlternateNpmRun.toBoolean()
     }
 
+    if (scopedPackage != null) {
+        isScopedPackage = scopedPackage.toBoolean()
+    }
 
-
-
-    //def artifactUrl = "${config.repoUrl}/${packageJSON.name}/${version}/${packageJSON.name}-${version}.tgz"
+    //Setting build environment variables
     def buildEnvVars = []
 
 
     echo "buildEnvVars.size: ${buildEnvVars.size()}"
+
+    buildEnvVars <<  [ name : NodejsConstants.NODEJS_PACKAGE_TAG_VARIABLE, value : config.package_tag ]
+    buildEnvVars <<  [ name : NodejsConstants.NODEJS_PACKAGE_TARBALL_VARIABLE, value : config.package_tarball ]
+    buildEnvVars <<  [ name : NodejsConstants.NODEJS_IS_SCOPED_PACKAGE_VARIABLE, value : config.is_scoped_package ]
 
     if (isDevMode) {
         buildEnvVars <<  [ name : NodejsConstants.DEV_MODE_ENVIRONMENT_VARIABLE, value : isDevMode ]
